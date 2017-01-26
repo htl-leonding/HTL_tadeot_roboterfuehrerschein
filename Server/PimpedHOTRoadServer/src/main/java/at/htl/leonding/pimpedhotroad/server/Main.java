0bee090e52df155f1a36b94dd3ac9215ff7daf25
Michael <michaelrynkiewicz3@gmail.com>
2017-01-26 21:46:07 +0100

    Added some nice output

--- Server/PimpedHOTRoadServer/src/main/java/at/htl/leonding/pimpedhotroad/server/Main.java
+++ Server/PimpedHOTRoadServer/src/main/java/at/htl/leonding/pimpedhotroad/server/Main.java
@@ -16,12 +16,22 @@ public class Main {
 //        VehicleServer cs = new VehicleServer(VehicleServer.DEFAULT_PORT,
 //                VehicleServer.DEFAULT_MUSIC_DIR);
 //        cs.start();
+        boolean printedRunning = false;
+
         VehicleServer.getInstance().setDaemon(false);
+        System.out.println("Starting server");
         VehicleServer.getInstance().start();
         while(true){
             while(VehicleServer.getInstance().isRunning() || VehicleServer.getInstance().isAlive()){
+                if(printedRunning == false) {
+                    System.out.println("Server running");
+                    printedRunning = true;
+                }
                 Thread.sleep(1000);
             }
+
+            printedRunning = false;
+            System.out.println("Server crashed. Restarting");
 
             VehicleServer.getInstance().start();
         }
